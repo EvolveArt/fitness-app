4f87b9188b1a11a2ba79fc1970380b84345064a3
Evolve <Artevolve@yahoo.com>
2019-11-21 20:06:47 +0100

    signupForm: email/pass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PastTrainingComponent } from "./training/past-training/past-training.co
 import { WelcomeComponent } from "./welcome/welcome.component";
 
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, AbstractControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
+  passMinLength = 5;
 
-  constructor() { }
+  signupForm = this.fb.group({
+    email: ["", [Validators.email, Validators.required]],
+    password: [
+      "",
+      [Validators.minLength(this.passMinLength), Validators.required]
+    ]
+  });
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {}
+
+  onSignUp() {
+    console.log(this.signupForm.get("email"));
   }
 
+  public get currentPassLength(): number {
+    return this.signupForm.get("password").value.length;
+  }
+
+  public get email(): AbstractControl {
+    return this.signupForm.get("email");
+  }
 }
